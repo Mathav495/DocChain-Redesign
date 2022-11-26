a2fd10c772bafd9fe4f6bb64d58998718d1e12ae
MATHAVAN <mathavanselva495@gmail.com>
2022-11-26 11:49:02 +0530

    axios module installed

--- client/src/pages/Publishdoc.svelte
+++ client/src/pages/Publishdoc.svelte
@@ -1,14 +1,14 @@
 <script>
-let fileName
+  let fileName
+  import axios from "axios"
+  import { onMount } from "svelte"
 
-const submitdata = () => {
-  console.log(fileName)
-}
+  onMount(() => {})
 
+  const submitdata = () => {
+    console.log(fileName)
+  }
 </script>
-
-
-
 
 <h1 class="text-3xl font-bold p-3 underline text-blue-400">Publish Doc</h1>
 <div class="bg-slate-200 w-2/5 mx-5 p-2 rounded-lg">
@@ -17,12 +17,14 @@ const submitdata = () => {
     id="form"
     method="post"
     action="/docs/initiate"
-    enctype="multipart/form-data">
+    enctype="multipart/form-data"
+  >
     <input
       bind:value={fileName}
       type="file"
       name="userimage"
-      accept="image/*,.pdf" />
+      accept="image/*,.pdf"
+    />
     <button class="bg-slate-400 px-2 py-1 rounded-lg">Upload</button>
   </form>
 </div>
