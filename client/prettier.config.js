559cd06455367e1482fd36ee168ce82b6b41cc46
venkatesh c <venky26494@yahoo.com>
2022-11-26 17:45:47 +0530

    prettier file added

---
+++ client/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 500,
+  // tabWidth: 4,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  semi: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  // svelteSortOrder: markup - styles - scripts
+};
