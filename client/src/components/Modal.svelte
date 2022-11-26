502190ce31020be49611007d756f6a7af3951fc9
venkatesh c <venky26494@yahoo.com>
2022-11-26 18:36:17 +0530

    modal has been integrated in filepreview page

--- client/src/components/Modal.svelte
+++ client/src/components/Modal.svelte
@@ -1,54 +1,26 @@
-<div class="relative max-w-md mx-auto mt-20">
-  <div
-    class="absolute z-10 inset-0 bg-gradient-to-r from-cyan-500 to-emerald-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-lg"
-  />
-  <div
-    class="relative z-20 bg-gradient-to-r from-purple-500 to-cyan-500 md:flex justify-between p-12 shadow-lg rounded-lg w-full max-w-2xl"
-  >
-    <div
-      class="sm:flex flex-col justify-between space-y-6 py-6 md:pr-10 mx-auto"
-    >
-      <!-- Modal header -->
-      <div
-        class="flex justify-between bg-slate-100 items-start p-4 rounded-t border-b w-full dark:border-gray-600"
-      >
-        <h3 class="text-2xl font-semibold text-gray-600 dark:text-white">
-          Sign Document:
-        </h3>
-        <button
-          type="button"
-          class="text-gray-900 bg-gray-200 hover:bg-red-600 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-toggle="defaultModal"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            /></svg
-          >
-          <span class="sr-only">Close modal</span>
-        </button>
-      </div>
-      <!-- Modal body -->
-      <div class="p-6 space-y-6">
-        <p
-          class="text-base font-medium leading-relaxed text-white text-center dark:text-gray-400"
-        >
-          Your SignatureID is:
-        </p>
-        <div
-          class="w-full px-4 py-2 text-gray-900 font-bold text-lg  border-2 rounded-lg text-center "
-          id="sign"
-        >
-          SignatureID
+<script>
+  export let showModal = false;
+</script>
+
+{#if showModal}
+  <div class="relative max-w-md mx-auto mt-20">
+    <div class="absolute z-10 inset-0 bg-gradient-to-r from-cyan-500 to-emerald-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-lg" />
+    <div class="relative z-20 bg-gradient-to-r from-purple-500 to-cyan-500 md:flex justify-between p-12 shadow-lg rounded-lg w-full max-w-2xl">
+      <div class="sm:flex flex-col justify-between space-y-6 py-6 md:pr-10 mx-auto">
+        <!-- Modal header -->
+        <div class="flex justify-between bg-slate-100 items-start p-4 rounded-t border-b w-full dark:border-gray-600">
+          <h3 class="text-2xl font-semibold text-gray-600 dark:text-white">Sign Document:</h3>
+          <!-- <button type="button" class="text-gray-900 bg-gray-200 hover:bg-red-600 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+            <span class="sr-only">Close modal</span>
+          </button> -->
+        </div>
+        <!-- Modal body -->
+        <div class="p-6 space-y-6">
+          <p class="text-base font-medium leading-relaxed text-white text-center dark:text-gray-400">Your SignatureID is:</p>
+          <div class="w-full px-4 py-2 text-gray-900 font-bold text-lg  border-2 rounded-lg text-center " id="sign">SignatureID</div>
         </div>
       </div>
     </div>
   </div>
-</div>
+{/if}
--- client/src/pages/Filepreview.svelte
+++ client/src/pages/Filepreview.svelte
@@ -1,16 +1,22 @@
 <script>
-  import axios from "axios";
-  import { createEventDispatcher, onMount } from "svelte";
+  import axios from 'axios';
+  import { createEventDispatcher, onMount } from 'svelte';
+  import Modal from '../components/Modal.svelte';
   const dispatch = createEventDispatcher();
   let signature,
-    error = "";
+    error = '';
 
-  let Token = localStorage.getItem("token");
-  console.log("token", Token);
-  let dataHash = localStorage.getItem("datahash");
-  console.log("datahash", dataHash);
-  let documentID = localStorage.getItem("documentID");
-  console.log("documentID", documentID);
+  let showModal = false;
+  const toggleModal = () => {
+    showModal = !showModal;
+  };
+
+  let Token = localStorage.getItem('token');
+  console.log('token', Token);
+  let dataHash = localStorage.getItem('datahash');
+  console.log('datahash', dataHash);
+  let documentID = localStorage.getItem('documentID');
+  console.log('documentID', documentID);
 
   /**
    * getting filedetails to publish
@@ -21,13 +27,10 @@
       documentID: documentID,
       signature: signature,
     };
-    const { addfile } = await axios.post(
-      "https://test.swagger.print2block.in/docs/add-file",
-      data
-    );
+    const { addfile } = await axios.post('https://test.swagger.print2block.in/docs/add-file', data);
     console.log(addfile);
-    localStorage.setItem("filehash", addfile.fileHash);
-    let fileHash = localStorage.getItem("filehash");
+    localStorage.setItem('filehash', addfile.fileHash);
+    let fileHash = localStorage.getItem('filehash');
     console.log(fileHash);
   });
 
@@ -37,13 +40,10 @@
       fileHash: fileHash,
       dataHash: dataHash,
     };
-    const { sign } = await axios.post(
-      "https://localhost:8080/sign/<fileHash><dataHash>",
-      hash
-    );
+    const { sign } = await axios.post('https://localhost:8080/sign/<fileHash><dataHash>', hash);
     console.log(sign);
-    localStorage.setItem("signature", sign.signature);
-    let signature = localStorage.getItem("signature");
+    localStorage.setItem('signature', sign.signature);
+    let signature = localStorage.getItem('signature');
     console.log(signature);
   };
 
@@ -57,10 +57,7 @@
         documentID: documentID,
         signature: signature,
       };
-      const { data } = await axios.post(
-        "https://test.swagger.print2block.in/docs/publish",
-        sample
-      );
+      const { data } = await axios.post('https://test.swagger.print2block.in/docs/publish', sample);
       console.log(data);
 
       let getData = {
@@ -68,11 +65,11 @@
         sign: data.signature,
         // filehash: data.filehash,
       };
-      localStorage.setItem("dataKey", JSON.stringify(getData));
-      let doc = localStorage.getItem("dataKey");
+      localStorage.setItem('dataKey', JSON.stringify(getData));
+      let doc = localStorage.getItem('dataKey');
       console.log(doc);
 
-      dispatch("push", data);
+      dispatch('push', data);
       // element.classList.add("hidden");
       // let element2 = document.getElementById("Preview");
       // element2.classList.remove("hidden");
@@ -81,78 +78,63 @@
   };
 </script>
 
-<div class=" flex h-screen w-screen bg-transparent">
-  <div class=" mx-auto flex items-center md:flex-row">
-    <div class="overflow-x-auto">
-      <table class="text-md w-full text-left text-gray-500">
-        <thead class="bg-transparent text-base uppercase text-gray-300 ">
-          <tr>
-            <th scope="col" class="w-80 py-3 px-10 md:w-40"> Name </th>
-            <th scope="col" class="w-32 py-3 px-6"> : </th>
-            <th scope="col" class="w-80 py-3 px-10 md:w-40"> John </th>
-          </tr>
-        </thead>
-        <thead class="bg-transparent text-base uppercase text-gray-300 ">
-          <tr>
-            <th scope="col" class="w-80 py-3 px-10"> Type </th>
-            <th scope="col" class="w-32 py-3 px-6"> : </th>
+<Modal {showModal} on:click={toggleModal} />
+<main>
+  <div class=" flex h-screen w-screen bg-transparent">
+    <div class=" mx-auto flex items-center md:flex-row">
+      <div class="overflow-x-auto">
+        <table class="text-md w-full text-left text-gray-500">
+          <thead class="bg-transparent text-base uppercase text-gray-300 ">
+            <tr>
+              <th scope="col" class="w-80 py-3 px-10 md:w-40"> Name </th>
+              <th scope="col" class="w-32 py-3 px-6"> : </th>
+              <th scope="col" class="w-80 py-3 px-10 md:w-40"> John </th>
+            </tr>
+          </thead>
+          <thead class="bg-transparent text-base uppercase text-gray-300 ">
+            <tr>
+              <th scope="col" class="w-80 py-3 px-10"> Type </th>
+              <th scope="col" class="w-32 py-3 px-6"> : </th>
 
-            <th scope="col" class="w-80 py-3 px-10 "> Sample </th>
-          </tr>
-        </thead>
-      </table>
-      <div class="mt-5 flex justify-center">
-        <img
-          src={file}
-          alt="document"
-          class="relative mx-auto mt-10 h-[400px] w-[320px] rounded-md "
-        />
-        <img
-          class="absolute ml-40 h-16 w-16  rounded-lg"
-          src={qrcode}
-          alt="qrcode"
-        />
-      </div>
+              <th scope="col" class="w-80 py-3 px-10 "> Sample </th>
+            </tr>
+          </thead>
+        </table>
+        <div class="mt-5 flex justify-center">
+          <img src={file} alt="document" class="relative mx-auto mt-10 h-[400px] w-[320px] rounded-md " />
+          <img class="absolute ml-40 h-16 w-16  rounded-lg" src={qrcode} alt="qrcode" />
+        </div>
 
-      <div
-        class="mx-auto mt-10 flex-col items-center justify-center text-center "
-      >
-        <label for="signature" class="text-md block font-medium text-gray-500 "
-          >signature</label
-        >
-        <div class="mt-1">
-          <textarea
-            name="signature"
-            bind:value={signature}
-            class=" mt-5 w-full rounded-md border-2 
+        <div class="mx-auto mt-10 flex-col items-center justify-center text-center ">
+          <label for="signature" class="text-md block font-medium text-gray-500 ">signature</label>
+          <div class="mt-1">
+            <textarea
+              name="signature"
+              bind:value={signature}
+              class=" mt-5 w-full rounded-md border-2 
                border-gray-300 px-4 py-2
                  placeholder:text-lg 
                    focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            placeholder="ECDSA Signature obtained"
-          />
+              placeholder="ECDSA Signature obtained"
+            />
+          </div>
         </div>
-      </div>
-      <h1 class="text-md font-semibold text-rose-500">{error}</h1>
+        <h1 class="text-md font-semibold text-rose-500">{error}</h1>
 
-      <div class="mx-auto mt-10 flex justify-between">
-        <button
-          class="rounded-lg bg-teal-500 px-6 py-2 text-lg text-white"
-          on:click={getsignature}>get-sign</button
-        >
-        <button
-          href="/blockchain"
-          on:click={publishdoc}
-          class="rounded-lg bg-teal-500 px-6 py-2 text-lg 
+        <div class="mx-auto mt-10 flex justify-between">
+          <button class="rounded-lg bg-teal-500 px-6 py-2 text-lg text-white" on:submit={getsignature} on:click={toggleModal}>get-sign</button>
+          <button
+            href="/blockchain"
+            on:click={publishdoc}
+            class="rounded-lg bg-teal-500 px-6 py-2 text-lg 
                 text-white disabled:cursor-not-allowed disabled:bg-teal-200"
-        >
-          publish to blockchain
-        </button>
+          >
+            publish to blockchain
+          </button>
 
-        <button
-          class="rounded-lg bg-teal-500 px-6 py-2 text-lg text-white"
-          href="/user/publish">revoke</button
-        >
+          <button class="rounded-lg bg-teal-500 px-6 py-2 text-lg text-white" href="/user/publish">revoke</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
+</main>
