55fc39ab02e2e573a800eef8d605684c65929be6
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-26 10:28:23 +0530

    svelte routing installed

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+	import Loginpage from "./pages/Loginpage.svelte";
+	import Dashboard from "./pages/Dashboard.svelte";
+  import { Route, Router } from "svelte-routing"
+  export let url = ""
 
 </script>
 
-<main>
-	<h2 class="text-3xl font-bold p-4">kalaiyarasi</h2>
-</main>
+<Router {url}>
+  <main>
+    <Route path="/"><Loginpage /></Route>
+    <Route path="dash"><Dashboard /></Route>
+  </main>
+</Router>
 
 <style global lang="postcss">
 @tailwind base;
