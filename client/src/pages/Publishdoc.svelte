00a1cc43f7d714ed8ba6e59653e283bc86cb891f
MATHAVAN <mathavanselva495@gmail.com>
2022-11-26 12:49:56 +0530

    function created for documentid generation

--- client/src/pages/Publishdoc.svelte
+++ client/src/pages/Publishdoc.svelte
@@ -1,17 +1,49 @@
 <script>
-  let fileName
   import axios from "axios"
-  import { onMount } from "svelte"
-
-  onMount(() => {})
+  let fileName, docValue
 
   const submitdata = () => {
     console.log(fileName)
   }
+
+  const submitdocid = async () => {
+    console.log(docValue)
+    if (docValue == "no") {
+      const { data } = await axios.post(
+        "https://test.swagger.print2block.in/docs/initiate/?qrcode=true",
+        {
+          filename: "sampledoc",
+        },
+        {
+          headers: {
+            "x-access-token":
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTU4MWUyYWFlYWNmZjYzOTlkYjYxZSIsImlhdCI6MTY2OTQ0Mzc0MywiZXhwIjoxNjY5NTMwMTQzfQ.SiCIvSaCqk1lcYXv1ebtjD-r1n7UwBNGnGZAi-Hbxko",
+          },
+        }
+      )
+      console.log(data)
+    }
+  }
 </script>
 
 <h1 class="text-3xl font-bold p-3 underline text-blue-400">Publish Doc</h1>
-<div class="bg-slate-200 w-2/5 mx-5 p-2 rounded-lg">
+
+<div class="bg-slate-200 w-3/5 mx-5 p-2 rounded-lg">
+  <form on:submit|preventDefault={submitdocid}>
+    <div class="flex flex-col">
+      <label for="docid">Do you have Document ID</label>
+      <div class="flex mt-3 gap-5">
+        <select name="docid" class=" w-24 " bind:value={docValue}>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+        <button class="bg-slate-400 px-2 py-1 rounded-lg">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<div class="bg-slate-200 w-3/5 mx-5 mt-10 p-2 rounded-lg">
   <form
     on:submit|preventDefault={submitdata}
     id="form"
