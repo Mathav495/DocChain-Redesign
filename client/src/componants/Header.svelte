fdeb8b1ed174bc0291bfcd5a8b58c37f7269b54c
GOWTHAMAN R <pudukkottai>
2022-11-26 14:55:38 +0530

    header portion completed

--- client/src/componants/Header.svelte
+++ client/src/componants/Header.svelte
@@ -1,13 +1,78 @@
-<div class="flex justify-between items-center ">
-  <div class="relative">
-    <div class="flex flex-row items-center">
-      <img src="assets\profile.png" alt="profile" class="h-16 w-16 ml-1 absolute" />
-      <h1 class="flex flex-row px-3 py-2 items-center bg-indigo-50 rounded-full">
-        <p class="text-xl font-semibold ml-10">Joji</p>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mt-1 ml-2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>        
-      </h1>
+<script>
+let hidden = true
+const showCard = () => {
+  if (hidden == false) {
+    hidden = true
+  } else {
+    hidden = false
+  }
+}
+const hideCard = () => {
+  hidden = true
+}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="flex justify-between items-center" on:click|self={hideCard}>
+  <h1 class="text-3xl font-bold">DocChain.io</h1>
+
+  <div class="flex flex-row gap-5">
+    <div class="relative flex-flex-col">
+      <div class="flex flex-row items-center">
+        <img
+          src="assets\profile.png"
+          alt="profile"
+          class="h-16 w-16 ml-1 absolute cursor-pointer" />
+        <h1
+          class="flex flex-row h-10 px-3 items-center bg-indigo-50 rounded-full">
+          <p class="text-lg font-semibold ml-12">Joji V.</p>
+          <button on:click={showCard}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5 mt-1 ml-2">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+          </button>
+        </h1>
+      </div>
+      <div
+        class:hidden
+        class="bg-black shadow-md shadow-gray-700 text-white absolute top-12 px-3 py-2 flex flex-col items-center justify-center rounded-2xl w-36 card-width">
+        <p class="text-xs">Welcome,</p>
+        <h1 class="text-md font-semibold">Mr.Joji Varghese</h1>
+        <a
+          href="https://mail.google.com/mail/"
+          class="text-sm text-blue-400 underline">test@email.com</a>
+      </div>
     </div>
+    <a
+      href="/"
+      class="bg-indigo-50 flex items-center justify-center h-10 w-10 rounded-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+      </svg>
+    </a>
   </div>
 </div>
+
+<style>
+.card-width {
+  width: 9.15rem /* 144px */;
+}
+</style>
--- client/src/pages/Dashboard.svelte
+++ client/src/pages/Dashboard.svelte
@@ -2,16 +2,17 @@
 import Header from "../componants/Header.svelte"
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="h-screen w-screen p-1">
   <div class="bg-black dash-bg-edge text-white h-full w-full">
     <div class="flex flex-row h-full py-3">
-      <div class="width-1 bg-red-500/50 py-5 flex flex-col items-center">
+      <div class="width-1 py-5 flex flex-col items-center">
         <div
           class="bg-white w-1/2 logo-height rounded-3xl flex items-center justify-center">
           <img src="assets\logo.png" alt="logo" class="h-10 w-10 shrink-0" />
         </div>
       </div>
-      <div class="width-2 bg-white text-gray-700 content-bg-edge p-10 mr-3">
+      <div class="width-2 bg-white text-gray-700 content-bg-edge p-8 mr-3">
         <Header />
       </div>
     </div>
