bfdf549e1b62eb80d0ccb056a07792e8fb3f85f0
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-26 11:30:26 +0530

    login page design work started

--- client/src/pages/Loginpage.svelte
+++ client/src/pages/Loginpage.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold p-3 underline text-blue-400">Login page</h1>
+<div class="flex">
+<div class="w-2/5 h-screen bg-black">
+
+</div>
+<div class="w-3/5 h-screen bg-gradient-to-r from-gray-500  to-black">
+
+</div>
+</div>
