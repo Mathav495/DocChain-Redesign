f2dd2bf588cb3a077a7f1b76b6c95c7f779f6599
MATHAVAN <mathavanselva495@gmail.com>
2022-11-26 11:22:23 +0530

    simple file uploading form created

--- client/src/pages/Publishdoc.svelte
+++ client/src/pages/Publishdoc.svelte
@@ -1,1 +1,28 @@
+<script>
+let fileName
+
+const submitdata = () => {
+  console.log(fileName)
+}
+
+</script>
+
+
+
+
 <h1 class="text-3xl font-bold p-3 underline text-blue-400">Publish Doc</h1>
+<div class="bg-slate-200 w-2/5 mx-5 p-2 rounded-lg">
+  <form
+    on:submit|preventDefault={submitdata}
+    id="form"
+    method="post"
+    action="/docs/initiate"
+    enctype="multipart/form-data">
+    <input
+      bind:value={fileName}
+      type="file"
+      name="userimage"
+      accept="image/*,.pdf" />
+    <button class="bg-slate-400 px-2 py-1 rounded-lg">Upload</button>
+  </form>
+</div>
