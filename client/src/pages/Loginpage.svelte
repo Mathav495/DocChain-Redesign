fe8785e80bf5d7d5d705c4e0fb1465343daebe78
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-26 11:56:09 +0530

    docChainlogo and docChain title were placed

--- client/src/pages/Loginpage.svelte
+++ client/src/pages/Loginpage.svelte
@@ -1,8 +1,18 @@
 <div class="flex">
-<div class="w-2/5 h-screen bg-black">
+  <div class="w-2/5 p-10 h-screen bg-black">
+    <div class="flex">
+      <img
+        class=" mr-2 inline-block animate-pulse align-top "
+        src="assets\icon1.png"
+        alt="icon1"
+      />
+      <h1 class="text-2xl text-white font-normal">DocChain</h1>
+    </div>
 
-</div>
-<div class="w-3/5 h-screen bg-gradient-to-r from-gray-500  to-black">
+    <div>
+      <form />
+    </div>
+  </div>
 
-</div>
+  <div class="w-3/5 h-screen bg-gradient-to-r from-gray-500  to-black" />
 </div>
