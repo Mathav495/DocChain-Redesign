2e287b7c1521a31767bc08020b79155d94c40638
MATHAVAN <mathavanselva495@gmail.com>
2022-11-26 14:16:04 +0530

    simple form created for form uploading

--- client/src/pages/Publishdoc.svelte
+++ client/src/pages/Publishdoc.svelte
@@ -1,10 +1,6 @@
 <script>
   import axios from "axios"
-  let fileName, docValue, documentID
-
-  const submitdata = () => {
-    console.log(fileName)
-  }
+  let fileName, docValue, documentID, dateissued
 
   const submitdocid = async () => {
     console.log(docValue)
@@ -27,6 +23,31 @@
       console.log(documentID)
     }
   }
+
+  const onSubmitFile = async () => {
+    console.log("Data submitted", dateissued)
+    const form = document.getElementById("form")
+    const formData = new FormData(form)
+    console.log([...formData])
+    let datum = [...formData][0]
+    let File = datum[1]
+    console.log(File)
+    const { data } = await axios.post(
+      "https://test.swagger.print2block.in/docs/add-file",
+      {
+        documentID: documentID,
+        file: File,
+      },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "x-access-token":
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTU4MWUyYWFlYWNmZjYzOTlkYjYxZSIsImlhdCI6MTY2OTQ1MDUwNiwiZXhwIjoxNjY5NTM2OTA2fQ.f-jWsn2PjD7vjWX-cW_GCXuZnMUEtnJvRr51w0bZ2tw",
+        },
+      }
+    )
+    console.log(data)
+  }
 </script>
 
 <h1 class="text-3xl font-bold p-3 underline text-blue-400">Publish Doc</h1>
@@ -48,7 +69,7 @@
 
 <div class="bg-slate-200 w-3/5 mx-5 mt-10 p-2 rounded-lg">
   <form
-    on:submit|preventDefault={submitdata}
+    on:submit|preventDefault={onSubmitFile}
     id="form"
     method="post"
     action="/docs/initiate"
@@ -61,5 +82,20 @@
       accept="image/*,.pdf"
     />
     <button class="bg-slate-400 px-2 py-1 rounded-lg">Upload</button>
+  </form>
+</div>
+
+<div class="bg-slate-200 w-3/5 mx-5 mt-5 p-2 rounded-lg">
+  <form>
+    <div class="flex flex-col">
+      <label for="docid">Date issued</label>
+      <input
+        bind:value={dateissued}
+        type="datetime-local"
+        name="docid"
+        id="docid"
+      />
+      <button>Submit</button>
+    </div>
   </form>
 </div>
