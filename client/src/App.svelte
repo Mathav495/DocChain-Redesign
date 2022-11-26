5a878a203ff66720e4122d6f50513212db93b988
MATHAVAN <mathavanselva495@gmail.com>
2022-11-26 11:11:49 +0530

    route created for publishdoc page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,6 +2,7 @@
 	import Loginpage from "./pages/Loginpage.svelte";
 	import Dashboard from "./pages/Dashboard.svelte";
   import { Route, Router } from "svelte-routing"
+  import Publishdoc from "./pages/Publishdoc.svelte"
   export let url = ""
 
 </script>
@@ -10,6 +11,7 @@
   <main>
     <Route path="/"><Loginpage /></Route>
     <Route path="/dash"><Dashboard /></Route>
+    <Route path="/publish"><Publishdoc /></Route>
   </main>
 </Router>
 
