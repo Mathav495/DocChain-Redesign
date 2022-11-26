2494d8788014e0254b490be6bee9b1474dc942db
GOWTHAMAN R <pudukkottai>
2022-11-26 12:50:59 +0530

    logo added in navbar

--- client/src/pages/Dashboard.svelte
+++ client/src/pages/Dashboard.svelte
@@ -1,8 +1,15 @@
 <div class="h-screen w-screen p-1">
-  <div class="bg-black dash-bg-edge text-white text-center h-full w-full">
-    <div class="flex flex-row h-full p-3">
-      <div class="width-1 bg-red-500 py-5">nav</div>
-      <div class="width-2 w bg-white content-bg-edge py-5">cont</div>
+  <div class="bg-black dash-bg-edge text-white h-full w-full">
+    <div class="flex flex-row h-full py-3">
+      <div class="width-1 bg-red-500/50 py-5 flex flex-col items-center">
+        <div
+          class="bg-white w-1/2 logo-height rounded-3xl flex items-center justify-center">
+          <img src="assets\logo.png" alt="logo" class="h-10 w-10 shrink-0" />
+        </div>
+      </div>
+      <div class="width-2 bg-white text-gray-700 content-bg-edge py-5 mr-3">
+        cont
+      </div>
     </div>
   </div>
 </div>
@@ -15,9 +22,12 @@
   border-radius: 3rem;
 }
 .width-1 {
-  width: 12%;
+  width: 10%;
 }
 .width-2 {
-  width: 88%;
+  width: 90%;
+}
+.logo-height {
+  height: 4.5rem /* 64px */;
 }
 </style>
