d90236bb3d60c36d73825f07189c25baf31645fe
GOWTHAMAN R <pudukkottai>
2022-11-26 12:24:45 +0530

    navbar and comtent pages are separated

--- client/src/pages/Dashboard.svelte
+++ client/src/pages/Dashboard.svelte
@@ -1,11 +1,23 @@
-<div class="h-screen w-screen p-2">
-  <div class="bg-black round text-white text-center h-full w-full">
-    dashboard
+<div class="h-screen w-screen p-1">
+  <div class="bg-black dash-bg-edge text-white text-center h-full w-full">
+    <div class="flex flex-row h-full p-3">
+      <div class="width-1 bg-red-500 py-5">nav</div>
+      <div class="width-2 w bg-white content-bg-edge py-5">cont</div>
+    </div>
   </div>
 </div>
 
 <style>
-.round {
+.dash-bg-edge {
   border-radius: 3rem;
+}
+.content-bg-edge {
+  border-radius: 3rem;
+}
+.width-1 {
+  width: 12%;
+}
+.width-2 {
+  width: 88%;
 }
 </style>
