0e28b7e5f1779cfce22c2a98a2d8c3bf4d82f693
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-28 15:24:30 +0530

    instruction card created

--- client/src/components/docIdform.svelte
+++ client/src/components/docIdform.svelte
@@ -26,32 +26,48 @@
 </script>
 
 <h1 class="text-xl font-bold tracking-wide">Publish Documents</h1>
-
-<div
-  class="bg-blue-100 shadow-sm shadow-black mt-3 w-full lg:w-3/5 p-5 rounded-lg"
->
-  <form on:submit|preventDefault={submitdocid}>
-    <div class="flex flex-col">
-      <label
-        for="docid"
-        class="text-lg font-semibold tracking-wide text-blue-600"
-        >Do you have Document ID</label
-      >
-      <div class="flex mt-3 gap-5">
-        <select
-          name="docid"
-          class="w-1/2 pl-2 rounded-lg"
-          bind:value={docValue}
+<div class="flex gap-5">
+  <div
+    class="bg-blue-100 shadow-sm shadow-black mt-5 w-full lg:w-3/5 p-4 rounded-lg"
+  >
+    <form on:submit|preventDefault={submitdocid}>
+      <div class="flex flex-col">
+        <label
+          for="docid"
+          class="text-lg  mt-4 font-semibold tracking-wide text-blue-600"
+          >Do you have Document ID</label
         >
-          <option class="pl-5">select</option>
-          <option value="yes">Yes</option>
-          <option value="no">No</option>
-        </select>
-        <button
-          class="flex text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
-          >Submit</button
-        >
+        <div class="flex mt-5 gap-5">
+          <select
+            name="docid"
+            class="w-1/2 pl-2 rounded-lg"
+            bind:value={docValue}
+          >
+            <option class="pl-5">select</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+          <button
+            class="flex text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+            >Submit</button
+          >
+        </div>
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
+
+  <div
+    class="bg-emerald-100 shadow-sm shadow-black mt-5 w-full lg:w-2/5 p-4 rounded-lg"
+  >
+    <h1 class="text-xl text-emerald-600 font-semibold tracking-wide">NOTE.</h1>
+    <ul class="list-disc list-outside p-3">
+      <li class="text-xl text-emerald-600 font-semibold tracking-wide">
+        If you don't have Document ID,Select 'NO' .It will generate Document ID.
+      </li>
+
+      <li class="text-xl text-emerald-600 font-semibold tracking-wide mt-1">
+        Otherwise select 'YES'
+      </li>
+    </ul>
+  </div>
 </div>
