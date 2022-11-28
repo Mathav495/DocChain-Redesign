0c6721ca4427556fd00ff6c8d22044877436d73d
GOWTHAMAN R <pudukkottai>
2022-11-28 14:18:59 +0530

    pagination component created

---
+++ client/src/components/pagination.svelte
@@ -0,0 +1,10 @@
+<div class="flex items-start justify-center p-5">
+  <div>
+    <nav class="flex rounded-md shadow-sm">
+      <a href="/Dash" aria-current="page" class="items-center border border-gray-300 bg-white/50 px-2 py-1 text-xs font-medium text-gray-900 hover:border-red-600 hover:bg-gray-500 hover:text-red-600 md:px-4 md:py-2">All</a>
+      <a href="/Dash" class="items-center border border-gray-300 bg-white/50 px-2 py-1 text-xs font-medium text-gray-900 hover:border-red-600 hover:bg-gray-500 hover:text-red-600 md:px-4 md:py-2">6m</a>
+      <a href="/Dash" class="items-center border border-gray-300 bg-white/50 px-2 py-1 text-xs font-medium text-gray-900 hover:border-red-500 hover:bg-gray-500 hover:text-red-600 md:px-4 md:py-2">1m</a>
+      <a href="/Dash" class="items-center border border-gray-300 bg-white/50 px-2 py-1 text-xs font-medium text-gray-900 hover:border-red-500 hover:bg-gray-500 hover:text-red-600 md:px-4 md:py-2">7d</a>
+    </nav>
+  </div>
+</div>
