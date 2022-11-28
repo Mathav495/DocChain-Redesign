5db6530a2a942302bb8ea02ea0eb840ef9b2943f
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-28 10:53:14 +0530

    password icon page created

--- client/src/pages/Loginpage.svelte
+++ client/src/pages/Loginpage.svelte
@@ -2,6 +2,7 @@
   import axios from "axios"
   import { navigate } from "svelte-routing"
   import Emailicon from "../icons/Emailicon.svelte"
+  import PasswordIcons from "../icons/Password.svelte"
   let type = "password"
   let Email = "",
     Password = ""
@@ -105,17 +106,7 @@
           >
           <div class="relative">
             <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                class="w-5 h-5 fill-gray-400 mt-2 group-hover:fill-blue-700"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <PasswordIcons />
             </span>
             {#if type == "password"}
               <input
