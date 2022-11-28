11c5f54d9d2a6d5e5633dad4cbb7d7ec46849237
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-28 10:55:14 +0530

    Eye icon page created

--- client/src/pages/Loginpage.svelte
+++ client/src/pages/Loginpage.svelte
@@ -2,6 +2,7 @@
   import axios from "axios"
   import { navigate } from "svelte-routing"
   import Emailicon from "../icons/Emailicon.svelte"
+  import Eye from "../icons/Eye.svelte"
   import PasswordIcons from "../icons/Password.svelte"
   let type = "password"
   let Email = "",
@@ -131,25 +132,7 @@
               class="absolute inset-y-0 right-4 flex items-center pl-2"
             >
               {#if type == "password"}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6 fill-gray-400 mt-2  group-hover:fill-blue-700"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
+                <Eye />
               {:else}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
