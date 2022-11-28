13a3b30780d224bc85f2384884c39e6bed17c537
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-11-28 10:47:18 +0530

    password toggle added

--- client/src/pages/Loginpage.svelte
+++ client/src/pages/Loginpage.svelte
@@ -1,6 +1,7 @@
 <script>
   import axios from "axios"
   import { navigate } from "svelte-routing"
+  let type = "password"
   let Email = "",
     Password = ""
   let error = {
@@ -45,6 +46,13 @@
       if (token) {
         navigate("/dash")
       }
+    }
+  }
+  const showPassword = () => {
+    if (type == "password") {
+      type = "text"
+    } else {
+      type = "password"
     }
   }
 </script>
@@ -119,14 +127,65 @@
                 />
               </svg>
             </span>
+            {#if type == "password"}
+              <input
+                bind:value={Password}
+                type="password"
+                id="Password"
+                placeholder="Enter your Password..."
+                class="w-full mt-2 bg-black pl-10 placeholder:text-sm focus:bg-black text-blue-500 rounded border border-gray-300 focus:border-white focus:ring-1 focus:ring-white  text-lg outline-none py-1 px-3 leading-8"
+              />
+            {:else}
+              <input
+                bind:value={Password}
+                type="text"
+                id="Password"
+                placeholder="Enter your Password..."
+                class="w-full mt-2 bg-black pl-10 placeholder:text-sm focus:bg-black text-blue-500 rounded border border-gray-300 focus:border-white focus:ring-1 focus:ring-white  text-lg outline-none py-1 px-3 leading-8"
+              />
+            {/if}
 
-            <input
-              bind:value={Password}
-              type="Password"
-              id="Password"
-              placeholder="Enter your Password..."
-              class="w-full mt-2 bg-black pl-10 placeholder:text-sm focus:bg-black text-blue-500 rounded border border-gray-300 focus:border-white focus:ring-1 focus:ring-white  text-lg outline-none py-1 px-3 leading-8"
-            />
+            <button
+              on:click={showPassword}
+              class="absolute inset-y-0 right-4 flex items-center pl-2"
+            >
+              {#if type == "password"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 fill-gray-400 mt-2  group-hover:fill-blue-700"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              {:else}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 fill-gray-400 mt-2  group-hover:fill-blue-700"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              {/if}
+            </button>
           </div>
 
           <h1 class="pt-1 text-sm font-semibold text-blue-500 md:text-base">
@@ -157,7 +216,5 @@
     </div>
   </div>
 
-  <div
-    class="w-full lg:w-2/3 h-screen bg-gradient-to-r from-gray-500  to-black"
-  />
+  <div class="w-full lg:w-2/3 h-screen" />
 </div>
