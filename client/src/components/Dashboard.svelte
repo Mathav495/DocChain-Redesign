266bf500f6593f8cab3af8d4093dec2a77f619e1
GOWTHAMAN R <pudukkottai>
2022-11-30 11:36:51 +0530

    in dashboard margin and padding issues are fixed

--- client/src/components/Dashboard.svelte
+++ client/src/components/Dashboard.svelte
@@ -24,17 +24,17 @@
 </script>
 
 <div class="flex flex-col gap-10 justify-center">
-  <div class="flex flex-col lg:flex-row">
+  <div class="flex flex-col lg:flex-row gap-5">
     <div class="flex order-2 lg:order-1 flex-col w-full lg:w-6/12 gap-3">
       <h1 class="text-2xl font-bold mt-10 lg:mt-0">Documents</h1>
-      <div class="graph-width lg:w-5/6 md:w-full bg-blue-100 h-60 text-2xl rounded-3xl p-1 flex flex-col justify-end items-center font-bold text-blue-500">
+      <div class="graph-width md:w-full bg-blue-100 h-60 text-2xl rounded-3xl p-1 flex flex-col justify-end items-center font-bold text-blue-500">
         <div><Pagination /></div>
       </div>
     </div>
     <div class="lg:w-6/12 w-full order-1 lg:order-2 flex flex-col gap-3">
       <h1 class="text-2xl font-bold">Documents Status</h1>
       <div class="flex flex-row gap-5">
-        <div class="w-1/2 bg-red-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
+        <div class="w-2/6 bg-red-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
           <p class="text-xl font-bold text-center">Document <br /> Issued</p>
 
           <p class="text-xl font-bold text-center" />
@@ -45,10 +45,10 @@
                 <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
               </svg>
             </div>
-            <p class="text-4xl font-bold text-red-500">23</p>
+            <p class="text-4xl font-bold text-red-500">{usage.initiated}</p>
           </div>
         </div>
-        <div class="w-1/2 bg-yellow-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
+        <div class="w-2/6 bg-yellow-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
           <p class="text-xl font-bold text-center">Document <br /> Viewed</p>
 
           <div class="flex flex-row gap-2 items-center justify-between">
@@ -58,10 +58,10 @@
                 <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
               </svg>
             </div>
-            <p class="text-4xl font-bold text-yellow-500">23</p>
+            <p class="text-4xl font-bold text-yellow-500">{usage.published}</p>
           </div>
         </div>
-        <div class="w-1/2 bg-green-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
+        <div class="w-2/6 bg-green-100 h-60 rounded-3xl p-5 flex flex-col justify-between">
           <p class="text-xl font-bold text-center">Document <br />Verified</p>
 
           <div class="flex flex-row gap-2 items-center justify-between">
@@ -70,7 +70,7 @@
                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
               </svg>
             </div>
-            <p class="text-4xl font-bold text-green-500">23</p>
+            <p class="text-4xl font-bold text-green-500">{usage.revoked}</p>
           </div>
         </div>
       </div>
@@ -94,7 +94,7 @@
           <input type="date" class="h-8 rounded-r-xl border-2 border-black bg-white px-2 outline-none md:mb-0 md:w-52" />
         </div>
       </div>
-      <div class="flex flex-col justify-between h-full gap-2">
+      <div class="flex flex-col justify-between h-full">
         <div class="flex items-center font-bold gap-3">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
             <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z" clip-rule="evenodd" />
@@ -124,8 +124,8 @@
       </div>
     </div>
     <!-- </div> -->
-    <div class="w-4/12 bg-black rounded-xl text-white p-7 sm:hidden lg:flex flex-col gap-8">
-      <span class="leading-10 text-2xl font-bold"
+    <div class="w-4/12 bg-black rounded-xl text-white p-7 sm:hidden lg:flex flex-col gap-3">
+      <span class="leading-9 text-2xl font-bold"
         >The <div class="transform -skew-y-6 inline-flex">
           <span class="text-lg font-bold border-2 bg-gray-300 text-black hover:bg-black hover:text-gray-300 border-gray-300 rounded-lg px-2">Print2Block</span>
         </div>
@@ -142,8 +142,18 @@
 <style>
   @media (min-width: 1024px) {
     .graph-width {
-      width: 93%;
+      width: 100%;
     }
+  }
+
+  @media (min-width: 1024px) {
+    .lg\:w-8\/12 {
+      width: 67.4%;
+    }
+  }
+
+  .w-4\/12 {
+    width: 32.6%;
   }
 
   .h-60 {
