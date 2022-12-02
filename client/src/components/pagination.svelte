659ed0c4a106a66e8d84d9c61f03c731824a45e0
GOWTHAMAN R <pudukkottai>
2022-12-02 23:22:17 +0530

    navbar and dashboardpage width alocated

--- client/src/components/pagination.svelte
+++ client/src/components/pagination.svelte
@@ -1,4 +1,4 @@
-<div class="flex items-start justify-center p-5">
+<div class="flex items-start justify-center">
   <div>
     <nav class="flex rounded-md shadow-sm">
       <a href="/dash" aria-current="page" class="items-center border border-blue-300 bg-white/50 px-2 py-1 text-xs text-gray-900 hover:border-blue-600 hover:bg-blue-200 hover:text-white font-bold md:px-4 md:py-2">All</a>
--- client/src/pages/Dashboardpage.svelte
+++ client/src/pages/Dashboardpage.svelte
@@ -31,24 +31,28 @@
   <Loader />
 </div> -->
 
-<div class="h-screen relative lg:w-screen w-auto" id="homepage">
-  <div class="bg-black text-white h-full w-full py-3">
-    <div class="flex flex-row h-full w-full">
-      <div class="w-auto p-8 hidden md:flex flex-col items-center justify-between">
-        <Logo />
-        <div>
-          <Nav />
-        </div>
-        <Logout />
-      </div>
-      <div class="w-auto bg-white text-gray-900 rounded-md p-8 ml-3 md:ml-0 mr-3">
-        <Dashboard />
-      </div>
-    </div>
+<div class="bg-black text-white h-screen w-screen flex flex-row py-3">
+  <div class="width1 p-8 hidden md:flex flex-col items-center justify-between">
+    <Logo />
+    <!-- <div class="w-full"> -->
+    <Nav />
+    <!-- </div> -->
+    <Logout />
+  </div>
+  <div class="width2 bg-white text-gray-900 rounded-md p-8 ml-3 md:ml-0 mr-3">
+    <Dashboard />
   </div>
   <!--small screen navbar-->
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div class:hidden={hideNavbar} on:click|self={hideNav} class="bg-white/50 block md:hidden absolute inset-0 p-12">
+  <button class:hidden={hideNavbar} on:click|self={hideNav} class="bg-white/50 block md:hidden absolute inset-0 p-12">
     <SmallScreenNavbar />
-  </div>
+  </button>
 </div>
+
+<style>
+  .width1 {
+    width: 18%;
+  }
+  .width2 {
+    width: 82%;
+  }
+</style>
