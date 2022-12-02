a0fd43f8ae93bc039669ed4894b28e2071af2c81
venkatesh <venky26494@yahoo.com>
2022-12-02 11:57:50 +0530

    changed succes route path in the successmsg

--- client/src/components/successmsg.svelte
+++ client/src/components/successmsg.svelte
@@ -2,7 +2,7 @@
   export let proposedURL
 </script>
 
-<div class=" mt-5 w-full rounded-lg bg-black">
+<div class=" mt-7 w-full rounded-lg bg-black">
   <div class="mx-5 flex  items-center justify-center">
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +17,10 @@
     </svg>
     <div >
       <h1 class="mx-5 p-4 text-lg font-semibold text-green-300 ">
-        Document has been Published, Your DocURL is:  <a class="text-red-300 " href="{proposedURL}" rel="noreferrer" target="_blank" ><strong>{proposedURL}</strong></a> 
+        Document has been Published:  
+        <!-- <a class="text-red-300 " href="{proposedURL}" rel="noreferrer" target="_blank" ><strong>{proposedURL}</strong></a>  -->
+
+        <a class="text-red-300 " href="/block" rel="noreferrer" target="_blank" ><strong>Check your Document Status</strong></a>
       </h1>
     
     </div>
