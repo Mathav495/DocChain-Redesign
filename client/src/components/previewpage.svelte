ee19219ee1f9e50e7b9a8b37f98f4bb76f609d51
venkatesh <venky26494@yahoo.com>
2022-12-05 11:18:29 +0530

    issuer name update in document preview

--- client/src/components/previewpage.svelte
+++ client/src/components/previewpage.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte';
   let documentID = localStorage.getItem('documentID');
   let fileHash = localStorage.getItem('filehash');
   console.log('filehash', fileHash);
@@ -10,6 +11,18 @@
   console.log('qrcodocURL', proposedURL);
   let imgurl = localStorage.getItem('img');
   console.log('imgUrl', imgurl);
+
+  let issuerName;
+
+  onMount(async () => {
+    const { data } = await axios.get('https://test.swagger.print2block.in/account/user', {
+      headers: {
+        'x-access-token': token,
+      },
+    });
+    issuerName = data.userData.name;
+    console.log(data.userData.name);
+  });
 
   // getting signature Id from the user
   const getsignature = async () => {
@@ -28,42 +41,44 @@
   };
 </script>
 
-<section class="text-gray-600 relative">
-  <div class="container pt-2 mx-auto flex sm:flex-nowrap flex-wrap md:w-flex-col">
-    <div class="lg:w-3/5 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10  flex w-full flex-col space-y-4 lg:order-none lg:block ">
-      <img class="w-full h-full rounded-md " src={imgurl} alt="" />
+<!-- <section class="text-gray-600 relative"> -->
+<!-- <div class="rounded-lg bg-blue-100 p-4"> -->
+<div class="container pt-2 mx-auto flex md:flex-nowrap flex-wrap md:w-flex-col">
+  <div class="lg:w-3/5 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden md:mr-10 flex w-full md:flex-row space-y-4">
+    <img class="w-full h-full rounded-md " src={imgurl} alt="" />
+  </div>
+  <div class="lg:w-2/5 md:w-1/2 bg-blue-300 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 p-5 rounded-md shadow-lg">
+    <div class="flex mb-4">
+      <img class="h-24 w-24 overflow-hidden rounded object-cover object-center border-2 border-white" src={qr} alt="qrcode" />
+      <img class="h-24 w-24 ml-2 overflow-hidden rounded object-cover object-center border-2 border-white" src="/assets/sample.jpg" alt="logo" />
     </div>
-    <div class="lg:w-2/5 md:w-1/2 bg-blue-300 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 p-5 rounded-md shadow-lg">
-      <div class="flex mb-4">
-        <img class="h-24 w-24 overflow-hidden rounded object-cover object-center border-2 border-white" src={qr} alt="qrcode" />
-        <img class="h-24 w-24 ml-2 overflow-hidden rounded object-cover object-center border-2 border-white" src="/assets/sample.jpg" alt="logo" />
-      </div>
-      <hr class="w-full mb-4" />
+    <hr class="w-full mb-4" />
 
-      <div class="relative mb-4">
-        <div for="name" class="leading-7 text-md font-bold text-gray-600 uppercase">Issuer</div>
-        <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-sm text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">SIDBI (DEMO)</div>
-      </div>
-      <div class="relative mb-4">
-        <div for="name" class="leading-7 text-md font-bold  text-gray-600">DATE PUBLISHED</div>
-        <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-sm text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">Wed Aug 17 2022 22:49:04 GMT+0530 (India Standard Time)</div>
-      </div>
-      <div class="relative mb-4">
-        <div for="name" class="leading-7 text-md font-bold  text-gray-600">DOCUMENT_ID</div>
-        <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-sm text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{documentID}</div>
-      </div>
-      <div class="relative mb-4">
-        <div for="name" class="leading-7 text-md font-bold  text-gray-600">FILEHASH</div>
-        <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-sm text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{fileHash}</div>
-      </div>
-      <div class="relative mb-4">
-        <div for="name" class="leading-7 text-md font-bold  text-gray-600 uppercase">datahash</div>
-        <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-sm text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{dataHash}</div>
-      </div>
-      <div class="p-2 w-full flex justify-between">
-        <button class="flex mx-auto text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none  rounded-lg text-lg uppercase" on:click|preventDefault={getsignature}>Sign</button>
-        <button class="flex mx-auto text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none  rounded-lg text-lg uppercase">publish</button>
-      </div>
+    <div class="relative mb-4">
+      <div for="name" class="leading-7 text-md font-bold text-gray-600 uppercase">Issuer</div>
+      <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-md text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{issuerName}</div>
+    </div>
+    <div class="relative mb-4">
+      <div for="name" class="leading-7 text-md font-bold  text-gray-600">DATE PUBLISHED</div>
+      <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-md text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">Wed Aug 17 2022 22:49:04 GMT+0530 (India Standard Time)</div>
+    </div>
+    <div class="relative mb-4">
+      <div for="name" class="leading-7 text-md font-bold  text-gray-600">DOCUMENT_ID</div>
+      <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-md text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{documentID}</div>
+    </div>
+    <div class="relative mb-4">
+      <div for="name" class="leading-7 text-md font-bold  text-gray-600">FILEHASH</div>
+      <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-md text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{fileHash}</div>
+    </div>
+    <div class="relative mb-4">
+      <div for="name" class="leading-7 text-md font-bold  text-gray-600 uppercase">datahash</div>
+      <div type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300  text-md text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{dataHash}</div>
+    </div>
+    <div class="p-2 w-full flex justify-between">
+      <button class="flex mx-auto text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none  rounded-lg text-lg uppercase" on:click|preventDefault={getsignature}>Sign</button>
+      <button class="flex mx-auto text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none  rounded-lg text-lg uppercase">publish</button>
     </div>
   </div>
-</section>
+</div>
+<!-- </div> -->
+<!-- </section> -->
