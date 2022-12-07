d926ee99481cfdae90336b4e0b41279b167e395e
GOWTHAMAN R <pudukkottai>
2022-12-07 19:57:19 +0530

    navs font sizes are changed to base

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -15,7 +15,7 @@
   };
 </script>
 
-<nav class="lg:text-base text-sm font-semibold flex flex-col gap-4">
+<nav class="text-base font-semibold flex flex-col gap-4">
   <a href="/dash" class="group flex transition-all duration-100 ease-in-out flex-row gap-1 items-center hover:border-x-2 border-blue-500 px-2 py-1 rounded-md" id="nav1" on:click={() => selectNav(1)}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="lg:h-8 lg:w-8 h-6 w-6 flex-shrink-0 transition-all duration-100 ease-in-out rounded-md ">
       <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
