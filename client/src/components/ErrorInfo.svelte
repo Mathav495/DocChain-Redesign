6ffdea62b947fa6c5576841282e58e0416ad71ff
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-12-08 23:03:28 +0530

    Error message are displayed

---
+++ client/src/components/ErrorInfo.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { fade } from 'svelte/transition';
+  export let errormsg;
+</script>
+
+<div in:fade={{ duration: 2000 }} out:fade={{ duration: 2000 }} class="flex bg-red-500 justify-center items-center mt-8 gap-4 p-3 rounded-md">
+  <h1 class="text-xl text-white font-medium tracking-wide">
+    {errormsg}
+  </h1>
+</div>
