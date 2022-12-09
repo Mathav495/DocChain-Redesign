5ffa437f05981586c0ff85546645de10da5eda22
GOWTHAMAN R <pudukkottai>
2022-12-09 20:14:08 +0530

    white icon, mono font number, gray font color

--- client/src/pages/Dashboardpage.svelte
+++ client/src/pages/Dashboardpage.svelte
@@ -53,12 +53,12 @@
 </div> -->
 
 <div class:bg-black={Black} class:bg-yellow-700={Yellow} class:bg-red-700={Red} class="relative text-white h-screen w-screen flex flex-row py-4 pr-4">
-  <div class="lg:w-88 md:w-3/8 hidden p-8 md:flex flex-col items-start justify-between">
+  <div class="lg:w-88 md:w-3/8 hidden p-8 md:flex flex-col items-start gap-4 justify-between overflow-auto">
     <Logo />
     <Nav />
     <Logout on:theme={changeClr} />
   </div>
-  <div class="lg:w-full md:w-5/8 w-full flex flex-col gap-4 bg-white text-gray-900 rounded-md p-8 ml-2 md:ml-0">
+  <div class="lg:w-full md:w-5/8 w-full flex flex-col gap-4 bg-white text-gray-900 rounded-md p-8 ml-2 md:ml-0 overflow-auto">
     <div class="md:hidden block">
       <Header on:navShow={showNav} />
     </div>
