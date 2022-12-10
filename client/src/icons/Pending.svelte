15a74660a9080de6555c33e3c8e4dabd3aabaf93
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-12-10 15:15:31 +0530

    icon repalced (solid to outline)

--- client/src/icons/Pending.svelte
+++ client/src/icons/Pending.svelte
@@ -1,4 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden lg:block w-7 h-7">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden lg:block w-7 h-7 stroke-yellow-500">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block lg:hidden w-5 h-5">
