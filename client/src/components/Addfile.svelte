e8fe3a54abe59332ec95a64a145da33f68b718c2
Mathavan <118589467+Mathav495@users.noreply.github.com>
2022-12-10 22:49:35 +0530

    nextbtn made disable during lastpage

--- client/src/components/Addfile.svelte
+++ client/src/components/Addfile.svelte
@@ -212,13 +212,12 @@
             {/if}
 
             <h1 class="text-lg text-white font-bold">{currentpage}</h1>
-            {#if nextbtn}
-              <button on:click={nextpage}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-white">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
-              </button>
-            {/if}
+
+            <button on:click={nextpage} disabled={!nextbtn}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" class="w-6 h-6 {!nextbtn ? 'stroke-gray-600' : 'stroke-white'}">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
