219cb2f6bf28bd597c9e725977320646d6b8071f
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-12-10 22:10:36 +0530

    tooltip fixed for continue icon

--- client/src/components/docIdform.svelte
+++ client/src/components/docIdform.svelte
@@ -112,16 +112,19 @@
             </div>
             <div class="py-3 w-1/4  tracking-wider flex gap-2 justify-center text-slate-900">
               <button class="hidden lg:block text-slate-800 border-2 border-green-500  py-1 px-2 hover:text-white  justify-center items-center focus:outline-none hover:bg-green-600 rounded text-base">Continue</button>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 cursor-pointer fill-green-500 h-5 block lg:hidden">
-                  <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clip-rule="evenodd" />
-                </svg>
-              </button>
+              <div class="has-tooltip">
+                <span class="tooltip rounded text-xs p-1 bg-black text-white -ml-16">Continue</span>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 cursor-pointer fill-green-500 h-5 block lg:hidden">
+                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
 
               <button class="hidden lg:block text-slate-800 border-2 border-red-500 py-1 px-2  hover:text-white justify-center items-center focus:outline-none hover:bg-red-600 rounded text-base">Release</button>
 
               <div class="has-tooltip">
-                <span class="tooltip rounded text-xs p-1 bg-black text-white ml-5">Release</span>
+                <span class="tooltip rounded text-xs p-1 bg-black text-white ml-6">Release</span>
                 <button>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 fill-red-500 h-5 block lg:hidden">
                     <path
