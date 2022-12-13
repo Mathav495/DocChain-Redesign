4a53c800e8cdf2c8ff06841ea3a0feff7b0790d4
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-12-13 11:05:18 +0530

    release button text color changed

--- client/src/components/docIdform.svelte
+++ client/src/components/docIdform.svelte
@@ -121,7 +121,7 @@
                 </button>
               </div>
 
-              <button class="hidden lg:block text-slate-800 border-2 border-red-500 py-1 px-2  hover:text-white justify-center items-center focus:outline-none hover:bg-red-600 rounded text-base">Release</button>
+              <button class="hidden lg:block text-red-500 border-2 border-red-500 py-1 px-2  hover:text-white justify-center items-center focus:outline-none hover:bg-red-600 rounded text-base">Release</button>
 
               <div class="has-tooltip">
                 <span class="tooltip rounded text-xs p-1 bg-black text-white ml-6">Release</span>
