9bab19f64825b62f38012f6ed10c64ea09dbaa70
Mathavan <118589467+Mathav495@users.noreply.github.com>
2022-12-19 14:24:35 +0530

    delete functionality added for the receiver section

--- client/src/components/Fileupload.svelte
+++ client/src/components/Fileupload.svelte
@@ -14,7 +14,7 @@
   let valid, date, sampleData, options, errormsg;
   let receiver = [
     {
-      label: 1,
+      label: 0,
       labelName: 'Fieldname',
       inputvalue: '',
     },
@@ -244,6 +244,7 @@
       }
     });
     console.log(count);
+    console.log(receiver.length);
     if (count == 0) {
       nextData = {
         label: increment_value,
@@ -358,6 +359,11 @@
     temp2.classList.remove('flex');
     temp2.classList.add('hidden');
   };
+
+  const deletelabel = (type, value) => {
+    console.log(type, value);
+    receiver = receiver.filter((receiver) => receiver.label != value);
+  };
 </script>
 
 <div class="space-y-3 flex flex-col justify-center items-center">
@@ -387,7 +393,7 @@
                     <svg on:click={() => hideLabel_receiver(receiver.label)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden group-hover:block cursor-pointer w-5 h-5">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden {receiver.labelName == 'Fieldname' ? '' : 'group-hover:block'}  cursor-pointer w-5 h-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" on:click={() => deletelabel('receiver', receiver.label)} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden {receiver.labelName == 'Fieldname' ? '' : 'group-hover:block'}  cursor-pointer w-5 h-5">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
                   </label>
