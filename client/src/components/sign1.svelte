b9760a4483af6071833f4ddab89c9dd70e733962
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-12-23 20:14:44 +0530

    5th portion modal changed

--- client/src/components/sign1.svelte
+++ client/src/components/sign1.svelte
@@ -787,16 +787,41 @@
           >
             SIGN DETAILS
           </h1>
-          <div class="flex justify-center">
-            <div
-              class=" mt-5 w-1/2 flex justify-center bg-slate-100 rounded-lg"
-            >
-              <button
-                on:click={pdfPreview}
-                class="text-center text-lg font-semibold text-slate-800"
+          <div class="flex w-full flex-col p-5">
+            <div class="cursor-pointer">
+              <div
+                class="flex items-center justify-center rounded-md border-blue-600 py-5"
               >
-                click here!!! and Preview
-              </button>
+                <div class="space-y-1 text-center">
+                  <div class="flex flex-col text-base">
+                    <label
+                      for="file-upload"
+                      class="relative cursor-pointer rounded-md font-semibold text-blue-800"
+                      id="dropzone"
+                    >
+                      <!-- svelte-ignore a11y-click-events-have-key-events -->
+                      <svg
+                        on:click={pdfPreview}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="mx-auto h-14 w-14"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M9 13.5l3 3m0 0l3-3m-3 3v-6m1.06-4.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+                        />
+                      </svg>
+                      <button on:click={pdfPreview} class="hover:underline">
+                        Click here to preview and download
+                      </button>
+                    </label>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
